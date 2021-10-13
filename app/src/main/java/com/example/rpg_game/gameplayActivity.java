ffe345665e063a26165829c19b29249da880adef
dannass001 <84390119+dannass001@users.noreply.github.com>
2021-10-13 08:08:20 -0500

    created gameplay activity

---
+++ app/src/main/java/com/example/rpg_game/gameplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.rpg_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gameplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
