851a5f6532b3026eabb8dfc8ea33f22324e6c585
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2021-10-15 08:05:13 -0500

    Added instructions

--- app/src/main/java/com/example/rpg_game/instructionsActivity.java
+++ app/src/main/java/com/example/rpg_game/instructionsActivity.java
@@ -3,14 +3,24 @@ package com.example.rpg_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class InstructionsActivity extends AppCompatActivity {
+
+    private final String instructions = "gjknfgkndfgn";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        TextView instructionText = findViewById(R.id.instructionsText);
+        instructionText.setText(instructions);
     }
 
-    
+    public void setInstructions(View v){
+
+    }
 }
