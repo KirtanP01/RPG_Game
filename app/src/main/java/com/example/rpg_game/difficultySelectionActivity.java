10cac675219df329d140d3e5f00d6871f2f02051
dannass001 <84390119+dannass001@users.noreply.github.com>
2021-10-18 07:44:59 -0500

    added character images to gameplay activity

--- app/src/main/java/com/example/rpg_game/difficultySelectionActivity.java
+++ app/src/main/java/com/example/rpg_game/difficultySelectionActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DifficultySelectionActivity extends AppCompatActivity {
+public class difficultySelectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/rpg_game/gameplayActivity.java
+++ app/src/main/java/com/example/rpg_game/gameplayActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GameplayActivity extends AppCompatActivity {
+public class gameplayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
