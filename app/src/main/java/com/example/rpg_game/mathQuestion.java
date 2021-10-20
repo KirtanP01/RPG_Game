204b20f37b41e3427b75f3946aa83d8226db406f
dannass001 <84390119+dannass001@users.noreply.github.com>
2021-10-20 08:09:12 -0500

    created math question activity

---
+++ app/src/main/java/com/example/rpg_game/mathQuestion.java
@@ -0,0 +1,14 @@
+package com.example.rpg_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mathQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_question);
+    }
+}
