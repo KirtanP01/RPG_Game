3c4ae108919201568ed0fc54686d7e4af9b0a336
KevinZhou168 <84091414+KevinZhou168@users.noreply.github.com>
2021-10-29 07:59:52 -0500

    answer verification

--- app/src/main/java/com/example/rpg_game/mathQuestion.java
+++ app/src/main/java/com/example/rpg_game/mathQuestion.java
@@ -2,6 +2,7 @@ package com.example.rpg_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -16,48 +17,63 @@ import java.util.ArrayList;
 public class mathQuestion extends AppCompatActivity {
 
     int rand;
+    public boolean canAttack;
+    ArrayList<String> firstGradeQuestions = new ArrayList<String>();
+    ArrayList<String> firstGradeAnswers = new ArrayList<String>();
+    ArrayList<String> secondGradeQuestions = new ArrayList<String>();
+    ArrayList<String> secondGradeAnswers = new ArrayList<String>();
+    ArrayList<String> thirdGradeQuestions = new ArrayList<String>();
+    ArrayList<String> thirdGradeAnswers = new ArrayList<String>();
+    ArrayList<String> fourthGradeQuestions = new ArrayList<String>();
+    ArrayList<String> fourthGradeAnswers = new ArrayList<String>();
+    ArrayList<String> fifthGradeQuestions = new ArrayList<String>();
+    ArrayList<String> fifthGradeAnswers = new ArrayList<String>();
+    ArrayList<String> sixthGradeQuestions = new ArrayList<String>();
+    ArrayList<String> sixthGradeAnswers = new ArrayList<String>();
+    ArrayList bonusQuestion= new ArrayList();
+    ArrayList bonusQuestionAnswer = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_question);
         rand = (int)(Math.random() * 5);
-        ArrayList<String> firstGradeQuestions = new ArrayList<String>();
+
         firstGradeQuestions.add("2 + 2");
         firstGradeQuestions.add("1 + 3");
         firstGradeQuestions.add("1 + 2");
         firstGradeQuestions.add("4 + 2");
         firstGradeQuestions.add("3 + 3");
 
-        ArrayList<String> firstGradeAnswers = new ArrayList<String>();
+
         firstGradeAnswers.add("4");
         firstGradeAnswers.add("4");
         firstGradeAnswers.add("3");
         firstGradeAnswers.add("6");
         firstGradeAnswers.add("6");
 
-        ArrayList<String> secondGradeQuestions = new ArrayList<String>();
+
         secondGradeQuestions.add("11 + 2");
         secondGradeQuestions.add("4 + 9");
         secondGradeQuestions.add("7 + 5");
         secondGradeQuestions.add("12 + 8");
         secondGradeQuestions.add("13 + 5");
 
-        ArrayList<String> secondGradeAnswers = new ArrayList<String>();
+
         secondGradeAnswers.add("13");
         secondGradeAnswers.add("13");
         secondGradeAnswers.add("12");
         secondGradeAnswers.add("20");
         secondGradeAnswers.add("18");
 
-        ArrayList<String> thirdGradeQuestions = new ArrayList<String>();
+
         thirdGradeQuestions.add("6 * 2");
         thirdGradeQuestions.add("4 * 3");
         thirdGradeQuestions.add("4 * 9");
         thirdGradeQuestions.add("2 * 4");
         thirdGradeQuestions.add("8 * 6");
 
-        ArrayList<String> thirdGradeAnswers = new ArrayList<String>();
+
         thirdGradeAnswers.add("12");
         thirdGradeAnswers.add("12");
         thirdGradeAnswers.add("36");
@@ -65,7 +81,7 @@ public class mathQuestion extends AppCompatActivity {
         thirdGradeAnswers.add("48");
 
 
-        ArrayList<String> fourthGradeQuestions = new ArrayList<String>();
+
         fourthGradeQuestions.add("752 + 257");
         fourthGradeQuestions.add("375 + 308");
         fourthGradeQuestions.add("602 + 374");
@@ -73,148 +89,62 @@ public class mathQuestion extends AppCompatActivity {
         fourthGradeQuestions.add("586 + 206");
 
 
-        ArrayList<String> fourthGradeAnswers = new ArrayList<String>();
+
         fourthGradeAnswers.add("1009");
         fourthGradeAnswers.add("683");
         fourthGradeAnswers.add("976");
         fourthGradeAnswers.add("880");
         fourthGradeAnswers.add("792");
 
-        ArrayList<String> fifthGradeQuestions = new ArrayList<String>();
+
         fifthGradeQuestions.add("426 - 243");
         fifthGradeQuestions.add("718 - 342");
         fifthGradeQuestions.add("548 - 785");
         fifthGradeQuestions.add("391 - 688");
         fifthGradeQuestions.add("420 - 69");
 
-        ArrayList<String> fifthGradeAnswers = new ArrayList<String>();
+
         fifthGradeAnswers.add("183");
         fifthGradeAnswers.add("376");
         fifthGradeAnswers.add("-237");
         fifthGradeAnswers.add("-297");
         fifthGradeAnswers.add("351");
 
-        ArrayList<String> sixthGradeQuestions = new ArrayList<String>();
+
         sixthGradeQuestions.add("36 / 6");
         sixthGradeQuestions.add("40 / 8");
         sixthGradeQuestions.add("56 / 8");
         sixthGradeQuestions.add("35 / 7");
         sixthGradeQuestions.add("28 / 4");
 
-        ArrayList<String> sixthGradeAnswers = new ArrayList<String>();
+
         sixthGradeAnswers.add("6");
         sixthGradeAnswers.add("5");
         sixthGradeAnswers.add("7");
         sixthGradeAnswers.add("5");
         sixthGradeAnswers.add("7");
 
-        ArrayList bonusQuestion= new ArrayList();
+
         bonusQuestion.add("∫sin(x)dx");
 
-        ArrayList bonusQuestionAnswer = new ArrayList();
+
         bonusQuestionAnswer.add("-cos(x) + c");
 
         TextView questionTV = findViewById(R.id.mathQuestionTextView);
         questionTV.setText(firstGradeQuestions.get(rand));
     }
     public static void main(String[] args) {
-//        ArrayList<String> firstGradeQuestions = new ArrayList<String>();
-//        firstGradeQuestions.add("2 + 2");
-//        firstGradeQuestions.add("1 + 3");
-//        firstGradeQuestions.add("1 + 2");
-//        firstGradeQuestions.add("4 + 2");
-//        firstGradeQuestions.add("3 + 3");
-
-//        ArrayList<String> firstGradeAnswers = new ArrayList<String>();
-//        firstGradeAnswers.add("4");
-//        firstGradeAnswers.add("4");
-//        firstGradeAnswers.add("3");
-//        firstGradeAnswers.add("6");
-//        firstGradeAnswers.add("6");
-//
-//        ArrayList<String> secondGradeQuestions = new ArrayList<String>();
-//        secondGradeQuestions.add("11 + 2");
-//        secondGradeQuestions.add("4 + 9");
-//        secondGradeQuestions.add("7 + 5");
-//        secondGradeQuestions.add("12 + 8");
-//        secondGradeQuestions.add("13 + 5");
-//
-//        ArrayList<String> secondGradeAnswers = new ArrayList<String>();
-//        secondGradeAnswers.add("13");
-//        secondGradeAnswers.add("13");
-//        secondGradeAnswers.add("12");
-//        secondGradeAnswers.add("20");
-//        secondGradeAnswers.add("18");
-//
-//        ArrayList<String> thirdGradeQuestions = new ArrayList<String>();
-//        thirdGradeQuestions.add("6 * 2");
-//        thirdGradeQuestions.add("4 * 3");
-//        thirdGradeQuestions.add("4 * 9");
-//        thirdGradeQuestions.add("2 * 4");
-//        thirdGradeQuestions.add("8 * 6");
-//
-//        ArrayList<String> thirdGradeAnswers = new ArrayList<String>();
-//        thirdGradeAnswers.add("12");
-//        thirdGradeAnswers.add("12");
-//        thirdGradeAnswers.add("36");
-//        thirdGradeAnswers.add("8");
-//        thirdGradeAnswers.add("48");
-//
-//
-//        ArrayList<String> fourthGradeQuestions = new ArrayList<String>();
-//        fourthGradeQuestions.add("752 + 257");
-//        fourthGradeQuestions.add("375 + 308");
-//        fourthGradeQuestions.add("602 + 374");
-//        fourthGradeQuestions.add("329 + 551");
-//        fourthGradeQuestions.add("586 + 206");
-//
-//
-//        ArrayList<String> fourthGradeAnswers = new ArrayList<String>();
-//        fourthGradeAnswers.add("1009");
-//        fourthGradeAnswers.add("683");
-//        fourthGradeAnswers.add("976");
-//        fourthGradeAnswers.add("880");
-//        fourthGradeAnswers.add("792");
-//
-//        ArrayList<String> fifthGradeQuestions = new ArrayList<String>();
-//        fifthGradeQuestions.add("426 - 243");
-//        fifthGradeQuestions.add("718 - 342");
-//        fifthGradeQuestions.add("548 - 785");
-//        fifthGradeQuestions.add("391 - 688");
-//        fifthGradeQuestions.add("420 - 69");
-//
-//        ArrayList<String> fifthGradeAnswers = new ArrayList<String>();
-//        fifthGradeAnswers.add("183");
-//        fifthGradeAnswers.add("376");
-//        fifthGradeAnswers.add("-237");
-//        fifthGradeAnswers.add("-297");
-//        fifthGradeAnswers.add("351");
-//
-//        ArrayList<String> sixthGradeQuestions = new ArrayList<String>();
-//        sixthGradeQuestions.add("36 / 6");
-//        sixthGradeQuestions.add("40 / 8");
-//        sixthGradeQuestions.add("56 / 8");
-//        sixthGradeQuestions.add("35 / 7");
-//        sixthGradeQuestions.add("28 / 4");
-//
-//        ArrayList<String> sixthGradeAnswers = new ArrayList<String>();
-//        sixthGradeAnswers.add("6");
-//        sixthGradeAnswers.add("5");
-//        sixthGradeAnswers.add("7");
-//        sixthGradeAnswers.add("5");
-//        sixthGradeAnswers.add("7");
-//
-//        ArrayList bonusQuestion= new ArrayList();
-//        bonusQuestion.add("∫sin(x)dx");
-//
-//        ArrayList bonusQuestionAnswer = new ArrayList();
-//        bonusQuestionAnswer.add("-cos(x) + c");
 
     }
 
-//    public boolean submit(View v){
-//        EditText answer = findViewById(R.id.mathAnswerEditText);
-//        String ans = answer.getText().toString();
+    public void submit(View v){
+        EditText answer = findViewById(R.id.mathAnswerEditText);
+        String userAns = answer.getText().toString();
+        String realAns = firstGradeAnswers.get(rand);
+        canAttack = userAns.equals(realAns);
+        Intent intent = new Intent(this, GameplayActivity.class);
+        startActivity(intent);
+
 //        TextView problem = findViewById(R.id.mathQuestionTextView);
 //        String prob = problem.getText().toString();
 //
@@ -222,7 +152,7 @@ public class mathQuestion extends AppCompatActivity {
 //        ScriptEngine engine = mgr.getEngineByName("JavaScript");
 //        String infix = "3+2*(4+5)";
 //        System.out.println(engine.eval(infix));
-//    }
+    }
 
     // onCreate: display random problem
     // submit button
