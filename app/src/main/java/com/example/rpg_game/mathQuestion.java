6de3bf26253fdfef6b0813da28c948a5d7d18d2f
dannass001 <84390119+dannass001@users.noreply.github.com>
2021-11-01 08:14:47 -0500

    Update mathQuestion.java

--- app/src/main/java/com/example/rpg_game/mathQuestion.java
+++ app/src/main/java/com/example/rpg_game/mathQuestion.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
