55ec99beeddc5cc926577c075b77cb58d3858a13
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2021-11-02 08:14:21 -0500

    Changed names

--- app/src/main/java/com/example/rpg_game/GameOver.java
+++ app/src/main/java/com/example/rpg_game/GameOver.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Congratulations extends AppCompatActivity {
+public class GameOver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_congratulations);
+        setContentView(R.layout.activity_game_over);
     }
 }
