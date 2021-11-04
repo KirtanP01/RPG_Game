2ec837f49b9f5569463716f99156e30a1dee5baf
KevinZhou168 <84091414+KevinZhou168@users.noreply.github.com>
2021-11-04 08:19:06 -0500

    broken code 2.0

--- app/src/main/java/com/example/rpg_game/GameplayActivity.java
+++ app/src/main/java/com/example/rpg_game/GameplayActivity.java
@@ -23,7 +23,7 @@ public class GameplayActivity extends AppCompatActivity {
     ImageButton play_pause_button;
     int defeatCount;
     int playIndex;
-    double originalVHealth = VILLAIN.getHealth();
+    double originalVHealth;
     Intent i;
 
     @Override
@@ -36,64 +36,71 @@ public class GameplayActivity extends AppCompatActivity {
         villainView.setImageResource(CharacterSelectionActivity.VILLAIN.getImageID());
         heroHealth = findViewById(R.id.playerHealthText);
         villainHealth = findViewById(R.id.enemyHealthText);
-        heroHealth.setText((HERO.getHealth()) + "");
-        villainHealth.setText((VILLAIN.getHealth()) + "");
-        play = true;
-        playIndex = 0;
-        play_pause_button = findViewById(R.id.play_pause_button);
-        if (playIndex >= 1){
-            playGame();
-        }
+        originalVHealth = VILLAIN.getHealth();
+//        heroHealth.setText((HERO.getHealth()) + "");
+//        villainHealth.setText((VILLAIN.getHealth()) + "");
+        //updateHealth();
+        //play = true;
+//        playIndex = 0;
+        //play_pause_button = findViewById(R.id.play_pause_button);
+        playGame();
+//        if (playIndex >= 1){
+//            playGame();
+//        }
         /*
             This method automatically opens up the math question activity and asks the user a math question
          */
         // https://stackoverflow.com/questions/7965494/how-to-put-some-delay-in-calling-an-activity-from-another-activity
-        if (playIndex == 0){
-            playIndex++;
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    i= new Intent(GameplayActivity.this,mathQuestion.class);
-                    startActivity(i);
-                }
-            }, 5000);
-        }
-
-//        while (HERO.getHealth() > 0 && VILLAIN.getHealth() > 0){
-//            attack();
+//        if (playIndex == 0) {
+//            playIndex++;
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    i = new Intent(GameplayActivity.this, mathQuestion.class);
+//                    startActivity(i);
+//                }
+//            }, 5000);
 //        }
+
     }
 
-    public void playGame(){
-        if (HERO.getHealth() <= 0){
-            i= new Intent(this,GameOver.class);
-            startActivity(i);
-        }
-        if (defeatCount == 3){
-            i= new Intent(this,Congratulations.class);
-            startActivity(i);
-        }
-        while (HERO.getHealth()>0 && defeatCount < 3) {
-            Toast.makeText(this, "The villain dealt " + VILLAIN.getDamage() + " damage!", Toast.LENGTH_SHORT).show();
-            VILLAIN.dealDamage(HERO);
-            heroHealth.setText((HERO.getHealth()) + "");
-            if (VILLAIN.getHealth() <= 0) {
-                Toast.makeText(this, "The monster has been defeated! A new monster appeared!", Toast.LENGTH_SHORT).show();
-                defeatCount++;
-                HERO.setHealth(100);
-                VILLAIN.setHealth(originalVHealth + 5);
-                originalVHealth = VILLAIN.getHealth();
-                VILLAIN.setVillainDamage(VILLAIN.getDamage() + 5);
+        public void playGame () {
+            while (HERO.getHealth() > 0 && defeatCount < 3) {
+
+                i = new Intent(GameplayActivity.this, mathQuestion.class);
+                startActivity(i);
+                if (VILLAIN.getHealth() <= 0) {
+                    Toast.makeText(this, "The monster has been defeated! A new monster appeared!", Toast.LENGTH_SHORT).show();
+                    defeatCount++;
+                    if (defeatCount == 3) {
+                        Intent congrats = new Intent(this, Congratulations.class);
+                        startActivity(congrats);
+                    }
+                    HERO.setHealth(100);
+                    VILLAIN.setHealth(originalVHealth + 5);
+                    originalVHealth = VILLAIN.getHealth();
+                    VILLAIN.setVillainDamage(VILLAIN.getDamage() + 5);
+                }
+                //updateHealth();
+                VILLAIN.dealDamage(HERO);
+                updateHealth();
+                Toast.makeText(this, "The villain dealt " + VILLAIN.getDamage() + " damage!", Toast.LENGTH_SHORT).show();
+                if (HERO.getHealth() <= 0) {
+                    Intent gameOver = new Intent(this, GameOver.class);
+                    startActivity(gameOver);
+                }
             }
-            i= new Intent(GameplayActivity.this,mathQuestion.class);
-            startActivity(i);
         }
-    }
 
-    public void homePage(View v){
-        Intent homePage = new Intent(this, MainActivity.class);
-        startActivity(homePage);
-    }
+        public void updateHealth () {
+            heroHealth.setText((HERO.getHealth()) + "");
+            villainHealth.setText((VILLAIN.getHealth()) + "");
+        }
+
+        public void homePage (View v){
+            Intent homePage = new Intent(this, MainActivity.class);
+            startActivity(homePage);
+        }
 
 //    public void pauseMenu(View view)
 //    {
@@ -101,20 +108,18 @@ public class GameplayActivity extends AppCompatActivity {
 //        startActivity(pauseMenu);
 //    }
 
-    public void pauseGame(View v){
-        boolean play = true;
+        public void pauseGame (View v){
+            boolean play = true;
 
-        if(play){
-            onPause();
-            play = false;
-            play_pause_button.setImageResource(R.drawable.pauseicon);
-        }
-
-        else if(!play){
+            if (play) {
+                onPause();
+                play = false;
+                play_pause_button.setImageResource(R.drawable.pauseicon);
+            } else if (!play) {
                 onResume();
                 play = true;
                 play_pause_button.setImageResource(R.drawable.playicon);
-        }
+            }
 
+        }
     }
-}
