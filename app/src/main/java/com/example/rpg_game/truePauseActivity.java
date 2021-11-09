d66dcb1d71c3fb2e6daa8ed96e5a872df72cf0a4
dannass001 <daniel.r.nass@gmail.com>
2021-11-09 17:34:24 -0600

    working app

--- app/src/main/java/com/example/rpg_game/truePauseActivity.java
+++ app/src/main/java/com/example/rpg_game/truePauseActivity.java
@@ -2,13 +2,10 @@ package com.example.rpg_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
 import android.os.Bundle;
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
->>>>>>> 642244d24301f4aaea5dca7fd12c63157d2d359e
 
 public class truePauseActivity extends AppCompatActivity {
 
@@ -17,8 +14,6 @@ public class truePauseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_true_pause);
     }
-<<<<<<< HEAD
-=======
 
     public void continueGame(View v){
         Intent continueGame = new Intent(this, GameplayActivity.class);
@@ -29,5 +24,4 @@ public class truePauseActivity extends AppCompatActivity {
         Intent continueGame = new Intent(this, MainActivity.class);
         startActivity(continueGame);
     }
->>>>>>> 642244d24301f4aaea5dca7fd12c63157d2d359e
 }
